81fabbda1c05c6a964f6c0c56aa727211a940060
gbiele <guidopb@neuro-cognition.org>
2015-08-28 13:28:36 +0200

    clean code

--- stan_wiener_test.R
+++ stan_wiener_test.R
@@ -7,43 +7,24 @@ library(rstan)
 # beta (b): Initial bias Bias for either response (beta > 0.5 means bias towards "upper" response 'A'). 0 < beta < 1
 # delta (v): Drift rate Quality of the stimulus (delta close to 0 means ambiguous stimulus or weak ability). 0 < delta
 # tau (ter): Nondecision time + Motor response time + encoding time (high means slow encoding, execution). 0 < ter (in seconds)
-n_per_condition = 500
+n_per_condition = 250
 
-resp = rbind(rwiener(n_per_condition,delta = .2, alpha = 1.25, beta = .5, tau = .2),
-             rwiener(n_per_condition,delta = .2, alpha = 1.25, beta = .4, tau = .2),
-             rwiener(n_per_condition,delta = .3, alpha = 1.25, beta = .6, tau = .2),
-             rwiener(n_per_condition,delta = .2, alpha = 1.5, beta = .5, tau = .3))
+resp = rbind(rwiener(n_per_condition,delta = .5, alpha = 1.25, beta = .5, tau = .15),
+             rwiener(n_per_condition,delta = .5, alpha = 1.25, beta = .4, tau = .15),
+             rwiener(n_per_condition,delta = .7, alpha = 1.25, beta = .6, tau = .15),
+             rwiener(n_per_condition,delta = .5, alpha = 1.5, beta = .5, tau = .2))
 resp$condition  = sort(rep(1:4,n_per_condition))
 
 
-stan_data2 = list(Nu = sum(resp$resp == "upper"),
+stan_data = list(Nu = sum(resp$resp == "upper"),
                   Nl = sum(resp$resp == "lower"),
                   RTu = resp$q[resp$resp == "upper"],
                   RTl = resp$q[resp$resp == "lower"],
                   K = length(unique(resp$condition)),
-                  su = table(resp$condition[resp$resp == "upper"]),
-                  sl = table(resp$condition[resp$resp == "lower"]),
+                  su = as.vector(table(resp$condition[resp$resp == "upper"])),
+                  sl = as.vector(table(resp$condition[resp$resp == "lower"])),
                   minRT = min(resp[,1]))
 
-inits = list(alpha = c(1,1,1,1),
-             beta = c(.5,.5,.5,.5),
-             delta = c(0,0,0,0),
-             tau = c(.1,.1,.1,.1))
+m = stan_model(file = "stan_wiener_test.stan")
+fit = sampling(m,data = stan_data,iter = 400,seed = 1234)
 
-fit = stan(file = "stan_wiener_test2.stan",data = stan_data2,iter = 1000)
-fit = stan(fit = fit,data = stan_data2,init = list(inits,inits,inits,inits),iter = 1000)
-
-fit = stan(fit = fit,
-           data = stan_data2,
-           chains = 4,
-           init = list(inits,inits,inits,inits),
-           iter = 1000)
-
-
-
-RTs = resp[,1]
-RTs[resp[,2] == "lower"] = -RTs[resp[,2] == "lower"]
-breaks = seq(from = -5,to = 5,by = .2)
-breaks = breaks[(which(findInterval(breaks,sort(RTs)) == 1)[1]-1):
-                  which(findInterval(breaks,sort(RTs)) == length(RTs))[1]]
-hist(RTs,breaks = breaks)
--- stan_wiener_test.stan
+++ stan_wiener_test.stan
@@ -1,17 +1,45 @@
 data {
-  int<lower=0> Nu; // number of upper boundary responses
-  int<lower=0> Nl; // number of lower boundary responses
-  real RTu[Nu]; // upper boundart responde times
-  real RTl[Nl]; // upper boundart responde times
+  int<lower=0> Nu; // # of upper boundary responses
+  int<lower=0> Nl; // # of lower boundary responses
+  real RTu[Nu]; // upper boundary response times
+  real RTl[Nl]; // lower boundary response times
+  int<lower=0> K; // # of groups
+  int su[K]; // group sizes upper boundary responses
+  int sl[K]; // group sizes lower boundary responses
+  real minRT;
 }
+
 parameters {
-  real delta;
-  real<lower=0, upper=10> alpha;
-  real<lower=0, upper=1> beta;
-  real<lower=0, upper=1> tau;
+  real<lower=-2, upper=1> delta[K];
+  real<lower=0, upper=5> alpha[K];
+  real<lower=0, upper=1> beta[K];
+  /* upper boundary of tau must be smaller than minimum RT
+  to avoid zero likelihood for fast responses.
+  tau can for physiological reasone not be faster than 0.1 s.*/
+  real<lower=.1, upper=minRT> tau[K]; 
 }
 
 model {
-  RTu ~ wiener(alpha, tau, beta, delta);
-  RTl ~ wiener(alpha, tau, 1-beta, -delta);
+  int posu;
+  int posl;
+  posu <- 1;
+  posl <- 1;
+  /* beta and tau have a very restricted ranges
+  so uniform prior should be OK
+  tau ~ cauchy(0,.1); 
+  beta ~ beta(1.1,1.1);*/
+  
+  /*this are nor great priors for delta and alpha
+  they avoid impossible high parameter values
+  but they have the largest mass at/around 0
+  which are also unlike parameter values*/ 
+  delta ~ normal(0,2); 
+  alpha ~ normal(0,2);
+
+   for (k in 1:K){
+    segment(RTu, posu, su[k]) ~ wiener(alpha[k], tau[k], beta[k], delta[k]);
+    posu <- posu + su[k];
+    segment(RTl, posl, sl[k]) ~ wiener(alpha[k], tau[k], 1-beta[k], -delta[k]);
+    posl <- posl + sl[k];
+  }
 }
